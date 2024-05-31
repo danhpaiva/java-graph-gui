b80e801e3074a4a285c2418c94b1f07985f01fc1
Paiva <danpaiva@live.com>
2024-05-30 21:15:30 -0300

    refact: identação do código

--- graph/src/main/java/graph/App.java
+++ graph/src/main/java/graph/App.java
@@ -3,40 +3,37 @@ package graph;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 
-
-
 public class App {
 
 	public static void main(String[] args) {
 		System.setProperty("org.graphstream.ui", "swing");
 
-		 Graph graph = new SingleGraph("Grafo");
-		 graph.setAttribute("ui.stylesheet", "node{\n" +
-	                "    size: 60px, 60px;\n" +
-	                "    fill-color: #f7f7f0;\n" +
-	                "    text-mode: normal; \n" +
-	                "}");
-		
-		 graph.addNode("0");  
-		 graph.addNode("1");  
-		 graph.addNode("2");  
-		 graph.addNode("3");   
-		 graph.addNode("4");
-		 
-		 graph.addEdge("01", "0", "1"); 
-		 graph.addEdge("04", "0", "4"); 
-		 graph.addEdge("12", "1", "2"); 
-		 graph.addEdge("13", "1", "3"); 
-		 graph.addEdge("14", "1", "4"); 
-		 graph.addEdge("23", "2", "3"); 
-		 graph.addEdge("34", "3", "4"); 
+		Graph graph = new SingleGraph("Grafo");
+		graph.setAttribute("ui.stylesheet", "node{\n" +
+				"    size: 60px, 60px;\n" +
+				"    fill-color: #f7f7f0;\n" +
+				"    text-mode: normal; \n" +
+				"}");
 
-		 for (Node node : graph) {
-		        node.setAttribute("ui.label", node.getId());
-		    }
+		graph.addNode("0");
+		graph.addNode("1");
+		graph.addNode("2");
+		graph.addNode("3");
+		graph.addNode("4");
 
-		 graph.display();   
- 
+		graph.addEdge("01", "0", "1");
+		graph.addEdge("04", "0", "4");
+		graph.addEdge("12", "1", "2");
+		graph.addEdge("13", "1", "3");
+		graph.addEdge("14", "1", "4");
+		graph.addEdge("23", "2", "3");
+		graph.addEdge("34", "3", "4");
+
+		for (Node node : graph) {
+			node.setAttribute("ui.label", node.getId());
+		}
+
+		graph.display();
+
 	}
-
 }
